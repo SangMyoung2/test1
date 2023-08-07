67a5cec6071b3ba207eccd2ab9a0105dfcfc0a09
SangMyoung2 <115723991+SangMyoung2@users.noreply.github.com>
2023-08-08 01:20:48 +0900

    코드길게 해서 작성

--- CodingTest/src/com/levelOne/Test3.java
+++ CodingTest/src/com/levelOne/Test3.java
@@ -4,4 +4,15 @@ public class Test3 {
 	
 	//개별 올리기
 	String su33;
+	
+	//코드가 짧아서 길게 늘려보자
+	public void print() {
+		System.out.println("깃헙은 너무 어렵다.");
+	}
+	
+	public void print1() {
+		System.out.println("깃헙은 정말정말 어렵다.");
+	}
+	
+	
 }
--- CodingTest/src/com/levelOne/test2.java
+++ CodingTest/src/com/levelOne/test2.java
@@ -4,4 +4,8 @@ public class test2 {
 
 	//추가
 	String su33;
+	
+	public void print() {
+		System.out.println("깃헙은 너무 쉽다.");
+	}
 }
