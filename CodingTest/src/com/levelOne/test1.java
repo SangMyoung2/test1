04ae3d984895cd95d031b80ed7bef27e75059c56
JiHyoKun93 <124251690+JiHyoKun93@users.noreply.github.com>
2023-08-08 17:13:01 +0900

    Revert "Revert "empty test1""
    This reverts commit 34836cab71982ee2c8cca8aa85a4e08a3c1c62ce.

--- CodingTest/src/com/levelOne/test1.java
+++ CodingTest/src/com/levelOne/test1.java
@@ -2,15 +2,7 @@ package com.levelOne;
 
 public class test1 {
 	
-	//�닔�젙�븳寃�
-	String su1;
+
 	
-<<<<<<< HEAD
-	//수정했다
-	private String userId;
-=======
-	//여기는 재석이 수정함
-	String su2123;
-	//수정
->>>>>>> main
+	
 }
